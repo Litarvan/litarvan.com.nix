9e01339b65c4ff2cc217f8930643e0f9a863294d
TheShark34 <adrien1975@live.fr>
2018-05-17 20:11:23 +0200

    Small fix

--- services/pronote/default.nix
+++ services/pronote/default.nix
@@ -1,11 +1,11 @@
-{ pkgs }: {
+{ pkgs, node ? pkgs.nodejs-8_x }: {
     description = "Pronote API";
     after = [ "network.target" ];
 
     serviceConfig = {
         Type = "simple";
         User = "litarvan";
-        ExecStart = "${pkgs.nodejs-8_x}/bin/node ${pkgs.callPackage ./pronote.nix}/index.js";
+        ExecStart = "${pkgs.nodejs-8_x}/bin/node ${pkgs.callPackage ./pronote.nix {}}/index.js";
         Restart = "on-failure";
     };
 
