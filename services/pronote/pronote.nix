57f32da73b94f36bdfad0761174d950908af018a
TheShark34 <adrien1975@live.fr>
2018-05-17 20:12:37 +0200

    yes

--- services/pronote/pronote.nix
+++ services/pronote/pronote.nix
@@ -1,4 +1,4 @@
 { pkgs }: with pkgs; stdenv.mkDerivation rec {
     name = "pronote-api-170518";
-    src = "/home/litarvan/pronote-api";
+    src = "/home/litarvan/pronote-api/pronote-api-170518.tar.gz";
 }
