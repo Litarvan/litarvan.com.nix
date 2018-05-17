91d93cdffa27fc1776395f3bd9f17408eaa35f46
TheShark34 <adrien1975@live.fr>
2018-05-17 20:44:19 +0200

    .

--- services/pronote/pronote.nix
+++ services/pronote/pronote.nix
@@ -1,5 +1,5 @@
 { pkgs }: with pkgs; stdenv.mkDerivation rec {
     name = "pronote-api-170518";
     src = /home/litarvan/pronote-api/pronote-api-170518.tar.gz;
-    builder = "${pkgs.coreutils}/bin/true";
+    builder = ./build.sh;
 }
