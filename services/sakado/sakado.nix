78143fdf0cfa232a24d4b140c4a727c8d49edf42
TheShark34 <adrien1975@live.fr>
2018-05-18 00:07:31 +0200

    Sakado fixes

--- services/sakado/sakado.nix
+++ services/sakado/sakado.nix
@@ -9,7 +9,7 @@ in {
     src = fetchgit {
         url = "https://github.com/sakado-app/sakado-server.git";
         rev = "ae72e6dbd0955b311a0090dc933a01ff8689a688";
-        sha256 = "1zfvavfxxf7j0q1sj20qsw2rc2j8m5hj49dx10gjggzwav2qnw1p";
+        sha256 = "0njx9rxxjl2p02vf6dpkp0bhzk6fn34gwjnwm0r7rgvibnld74y1";
     };
     builder = ./build.sh;
     inherit unzip gradle coreutils;
